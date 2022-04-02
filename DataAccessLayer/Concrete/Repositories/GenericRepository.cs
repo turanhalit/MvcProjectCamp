17b4b415dfd6e401a1d6b100c4b1eb7588e5db11
Halit <turanhalittt@gmail.com>
2022-04-02 14:08:00 +0300

    Data Access Katmanı düzenlendi. Generic Repository eklendi

---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context context = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+        public void Delete(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/WriterRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class WriterRepository : IWriterDal 
+    {
+        Context context = new Context();
+        DbSet<Writer> _object;
+        public void Delete(Writer p)
+        {
+            _object.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void Insert(Writer p)
+        {
+            _object.Add(p);
+            context.SaveChanges();
+        }
+
+        public List<Writer> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<Writer> List(Expression<Func<Writer, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(Writer p)
+        {
+            context.SaveChanges();
+        }
+    }
+}
